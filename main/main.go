59092701ab8aff49ec27ae84487baccb78b8f62e
Айбар <aibar.alauov@narxoz.com>
2026-09-17 14:42:54 +0500

    add printter func

--- main/main.go
+++ main/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello bitlab")
+	var nums []int
+	nums = append(nums, 10)
+	nums = append(nums, 100)
+	nums = append(nums, 1000)
 }
---
+++ main/printer.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func Printer(nums []int) {
+	for i := 0; i < len(nums); i++ {
+		fmt.Println(nums[i])
+	}
+}
